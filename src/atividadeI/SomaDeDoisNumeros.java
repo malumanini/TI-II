beb1d6cef089c89e52cca9e0749743f3e22cd0d5
malumanini <marialuizam.deoliveira@gmail.com>
2025-08-26 22:01:54 -0300

    Atividade I

---
+++ src/atividadeI/SomaDeDoisNumeros.java
@@ -0,0 +1,25 @@
+package atividadeI;
+import java.util.*;
+
+class SomaDeDoisNumeros{
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String args[]) {
+		//declaração de váriaveos
+		int num1, num2, soma;
+		
+		//Leituras
+		System.out.println("Digite um número: ");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro número: ");
+		num2 = sc.nextInt();
+		
+		//Somar
+		soma = num1 + num2;
+		
+		//Mostrar na tela
+		System.out.println("Soma:"+soma);
+		
+	}
+
+}
